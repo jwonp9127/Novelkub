815af09333b47aaa637f3885c65a71485d310049
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-20 01:21:47 +0900

    [FIX] 코드고치기 (-재훈)
    코드고치기

--- Novelkub/Assets/Scripts/Nosook/Potal.cs
+++ Novelkub/Assets/Scripts/Nosook/Potal.cs
@@ -10,8 +10,7 @@ public class Potal : MonoBehaviour
     public GameObject InPotal;
     private Collider collider;
     int i = -1;
-    //public GameObject PubPotalIn;
-    // Start is called before the first frame update
+   
     void Start()
     {
         collider = InPotal.GetComponent<Collider>();
@@ -23,23 +22,15 @@ public class Potal : MonoBehaviour
     {
         
     }
-    //public void Teleport(Vector3 spawnPosition)
-    //{
-    //    Controller.enabled = false;
-    //    transform.position = spawnPosition;
-    //    Controller.enabled = true;
-    //}
-    //player.Teleport(PubPotal.transform.position);
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
            
             player.Teleport(PubPotal.transform.position + new Vector3(0,2,i));
-            Debug.Log("나 들어왔어");
             i = -i;
-            //other.gameObject.transform.position = PubPotal.transform.position;
-            //collider.enabled = false;
+            
 
         }
     }
