cc9fa42d9afaadc7db0aba562d66582118333481
SongJinx3512 <subsub3512@gmail.com>
2023-10-19 22:13:27 +0900

    back to basic pizza game

--- Novelkub/Assets/Scripts/pizzaGame/pizzaDeliveryPoint.cs
+++ Novelkub/Assets/Scripts/pizzaGame/pizzaDeliveryPoint.cs
@@ -1,60 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using System;
 
 public class pizzaDeliveryPoint : MonoBehaviour
 {
-    AudioSource audio;
-    public pizzaGameManager manager;
-    private float gameduration = 180.0f;
-    public TMP_Text timeText;
-
-    void Awake()
+    void OnTriggerEnter(Collider other)
     {
-        audio = GetComponent<AudioSource>();
-    }
-
-    private void Start()
-    {
-        UpdatetimeText();
-    }
-
-    private void UpdatetimeText()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void Update()
-    {
-        if (gameduration > 0.0f)
-        {
-            gameduration -= Time.deltaTime;
-            if (gameduration <= 0.0f)
-            {
-                //restartquest();
-            }
-            UpdatetimeText();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "point")
+        if (other.name == "Player")
         {
             Player player = other.GetComponent<Player>();
             player.pointCount++;
-            audio.Play();
             gameObject.SetActive(false);
         }
     }
-
-    private void updatetimertext()
-    {
-        int minutes = Mathf.FloorToInt(gameduration / 60);
-        int seconds = Mathf.FloorToInt(gameduration - minutes * 60);
-        timeText.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
-    }
 }
+
